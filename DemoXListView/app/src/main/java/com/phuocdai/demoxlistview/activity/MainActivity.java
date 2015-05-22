e0acbd0cbf511a076ab1b7da0b6a8d6c69130b51
daivp <daivp@aoi-sys.vn>
2015-05-22 17:37:58 +0700

    add list view and actionbar

--- DemoXListView/app/src/main/java/com/phuocdai/demoxlistview/activity/MainActivity.java
+++ DemoXListView/app/src/main/java/com/phuocdai/demoxlistview/activity/MainActivity.java
@@ -3,20 +3,23 @@ package com.phuocdai.demoxlistview.activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ListView;
 
 import com.phuocdai.demoxlistview.R;
 import com.phuocdai.demoxlistview.adapter.ProductAdapter;
 import com.phuocdai.demoxlistview.item.ProductItem;
+import com.phuocdai.demoxlistview.xlistview.XListView;
 
 import java.util.ArrayList;
 
 
 public class MainActivity extends BaseActivity {
 
-    private ListView lvProduct;
+    private XListView lvProduct;
     private ArrayList<ProductItem> mProductItems;
+    ArrayList<ProductItem> mProductItemsCurrent = new ArrayList<>();
     private ProductAdapter mProductAdapter;
+    private int mTotalProduct = 20;
+    private int mCurrentOffset = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,20 +30,83 @@ public class MainActivity extends BaseActivity {
     protected void setupView() {
         super.setupView();
         setContentView(R.layout.activity_main);
-        lvProduct = (ListView) findViewById(R.id.main_listview_product);
-        initData();
+        setupActionBar(true, true);
+        lvProduct = (XListView) findViewById(R.id.main_listview_product);
+//        initData();
+        setupProductListViewAdapter();
         setupProductListView();
+    }
+
+    /**
+     * setup product listview adapter
+     */
+    private void setupProductListViewAdapter() {
+        if (mProductAdapter == null) {
+            mProductAdapter = new ProductAdapter(mContext, mProductItems);
+        }
+        lvProduct.setAdapter(mProductAdapter);
     }
 
     /**
      * setup product listview
      */
     private void setupProductListView() {
-        if (mProductAdapter == null) {
-            mProductAdapter = new ProductAdapter(mContext, mProductItems);
-        }
-        lvProduct.setAdapter(mProductAdapter);
+        lvProduct.setPullRefreshEnable(true);
+        lvProduct.setPullLoadEnable(false);
+        lvProduct.setXListViewListener(new XListView.IXListViewListener() {
+            @Override
+            public void onRefresh() {
+                getListProduct();
+            }
+
+            @Override
+            public void onLoadMore() {
+                getLoadmoreProduct();
+            }
+        });
     }
+
+    /**
+     * get list product
+     */
+    public void getListProduct() {
+
+        try {
+            ShowDiaLogLoading();
+            Thread.sleep(4000);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        initData();
+        mProductItemsCurrent = new ArrayList<>();
+        mProductAdapter.setItems(mProductItemsCurrent);
+        mCurrentOffset = 0;
+        for (int i = 0; i<10 ;i++){
+            mProductItemsCurrent.add(mProductItems.get(i));
+        }
+        mCurrentOffset = 10;
+        mProductAdapter.setItems(mProductItemsCurrent);
+        lvProduct.stopLoadMore();
+        lvProduct.stopRefresh();
+        lvProduct.setPullLoadEnable(true);
+        dismissDialogLoading();
+    }
+
+    /**
+     * get load more product
+     */
+    private void getLoadmoreProduct() {
+
+        initData();
+        for (int i = mCurrentOffset; i<15; i++){
+            mProductItemsCurrent.add(mProductItems.get(i));
+        }
+        mCurrentOffset += 5;
+        mProductAdapter.setItems(mProductItemsCurrent);
+        lvProduct.stopLoadMore();
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -66,7 +132,7 @@ public class MainActivity extends BaseActivity {
 
     private void initData(){
         mProductItems = new ArrayList<>();
-        for(int i =0; i< 30 ;i++) {
+        for(int i =0; i< 20 ;i++) {
             ProductItem productItem = new ProductItem();
             productItem.setMaSP("00"+i);
             productItem.setTenSp("APPLE "+i);
